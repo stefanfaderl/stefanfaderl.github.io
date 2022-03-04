c4cb56d931732a008e1587018e5b56000da229d1
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-04 12:11:09 +0100

    update Projects

--- index.js
+++ index.js
@@ -98,3 +98,15 @@ img.forEach((n) =>
     window.open(url, "_blank");
   })
 );
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const textProjects = document.querySelector(".textProjects");
+
+readMoreBtn.addEventListener("click", (e) => {
+  textProjects.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
