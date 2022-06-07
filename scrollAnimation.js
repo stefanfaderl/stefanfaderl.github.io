aa58ad74d4f71ff6b2b4ba177dc27a248e5c322c
Stefan <btc.stefanfaderl@gmail.com>
2022-06-07 17:38:24 +0200

    project scroll animation

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -23,3 +23,29 @@ let timeline02 = gsap.timeline({
 });
 
 timeline02.from(".grid-container-about-2", { x: 100, autoAlpha: 0 });
+
+const flexItem = gsap.utils.toArray('[class*="flex-item"]');
+
+gsap.set(flexItem, { autoAlpha: 0, y: 140 });
+
+flexItem.forEach((item, i) => {
+  const anim = gsap.to(item, {
+    duration: 1.2,
+    autoAlpha: 1,
+    y: 0,
+    paused: true,
+  });
+
+  // Use callbacks to control the state of the animation
+  ScrollTrigger.create({
+    trigger: item,
+    start: "top center",
+    end: "bottom center",
+    once: true,
+    onEnter: (self) => {
+      // If it's scrolled past, set the state
+      // If it's scrolled to, play it
+      self.progress === 1 ? anim.progress(1) : anim.play();
+    },
+  });
+});
